fc79ca24843b8c280e44650f3cfac75a8e2414d4
Tarun Kumar <tarun.kumar.csbs25@heritageit.edu.in>
2023-09-26 00:16:34 +0530

    Update

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
+// if(proccess.env.NODE_ENV!=="production"){
+require('dotenv').config();
+
 
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/vehicle_tracking";
+// const MONGODB_URI = process.env.MONGODB_URI;
 const connectToMongo = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
